ff958c9e35126dbc5b25f59bf637f0161b53ec46
Francesco <francesco98m@gmail.com>
2022-06-07 17:54:11 +0200

    added map and exceptions handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,63 +1,59 @@
 import twint
 import streamlit as st
-from geopy.geocoders import Nominatim
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
+import folium as fl
+from streamlit_folium import st_folium
 
-def get_tweets(location,radius,N):
+def get_tweets(lat, lng, radius, N):
     c = twint.Config()
     c.Limit = N
     c.Popular_tweets = False
     c.Pandas = True
     c.Hide_output = True
-    c.Geo = str(location.latitude)+","+str(location.longitude)+","+str(radius)+"km"
+    c.Geo = str(lat)+","+str(lng)+","+str(radius)+"km"
 
     twint.run.Search(c)
     return True
 
-geolocator = Nominatim(user_agent="thunder.vault00@gmail.com")
 
-nation = ""
-city = ""
-street = ""
-number = ""
 submitted = False
 
 st.title("Geo-Trends")
-st.caption("Simply input a location specifying some of the following parameters to obtain an image with the most used hashtags in the tweets of the area")  
+st.caption("Choose a location on the map, specify a radius to search in and the number of tweets to scrape and wait for a graph of the most used hashtags in the area.")  
 
-with st.form("Input Desired Location"):
-    nation = st.text_input("Nation")
-    city = st.text_input("City")
-    street = st.text_input("Street")
-    number = st.text_input("Number")
+
+with st.form("Parameters"):
+    m = fl.Map()
+    m.add_child(fl.LatLngPopup())
+    map = st_folium(m, height=350, width=700)
+    
     tweets_n = st.slider("Number of tweets to search",10,5000,200,10)
     radius = st.slider("Radius around location to search (km)",1,10,2,1)
-    loc = street + " " + number + " " + city + " " + nation
-    location = geolocator.geocode(loc)
 
     submitted = st.form_submit_button("Submit")
     if submitted:
-        st.subheader("Location")
-        st.text(location.address)
-        st.subheader("Coordinates")
-        st.write(location.latitude, location.longitude)
+        st.caption('You choose to search %d tweets, in a radius of %d km centered in (%.5f,%.5f)'%(tweets_n, radius, map['last_clicked']['lat'], map['last_clicked']['lng']))
 
 if submitted:
     with st.spinner():
-        get_tweets(location,radius,tweets_n)
+        get_tweets(map['last_clicked']['lat'], map['last_clicked']['lng'], radius,tweets_n)
     
     tweets_df = twint.storage.panda.Tweets_df
 
-    hashtag_text = ""
-    for s in tweets_df['hashtags']:
-        if len(s) > 0:
-            for e in s:
-                hashtag_text += e + " "
-    wordcloud = WordCloud(width = 2000, height = 1500, random_state=1, background_color='black',
-                             collocations=False, stopwords = STOPWORDS).generate(hashtag_text)
-    fig, ax = plt.subplots()
-    ax.imshow(wordcloud)
-    ax.set_axis_off()
-    st.pyplot(fig)
+    if 'hashtags' in tweets_df:
+        hashtag_text = ""
+        for s in tweets_df['hashtags']:
+            if len(s) > 0:
+                for e in s:
+                    hashtag_text += e + " "
+        wordcloud = WordCloud(width = 2000, height = 1500, random_state=1, background_color='black',
+                                 collocations=False, stopwords = STOPWORDS).generate(hashtag_text)
+        fig, ax = plt.subplots()
+        ax.imshow(wordcloud)
+        ax.set_axis_off()
+        st.pyplot(fig)
+    else:
+        st.error("There were not hashtags in the region searched. Try to increase the number of tweets and/or the radius.")
+
 
